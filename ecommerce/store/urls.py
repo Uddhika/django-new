08820ea8a8c6611ad5bc6afc26327c45f4eb0e5d
Uddhika <uddhikaishara@gmail.com>
2022-02-01 10:19:42 +0530

    first commit

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.store,name="store"),
+    path('cart/',views.cart,name="cart"),
+    path('checkout/',views.checkout,name="checkout")
+    
+]
