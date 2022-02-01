fe8fc0c9576d42c538990077ca5aa71e42db209c
Uddhika <uddhikaishara@gmail.com>
2022-02-01 17:50:45 +0530

    add to cart data pass

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,8 @@
 
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import *
+import json
 
 def store(request):
     products=Product.objects.all()
@@ -36,3 +38,32 @@ def checkout(request):
 
     context={'items': items, 'order':order}
     return render(request, 'store/checkout.html',context)
+
+
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId=data['productId']
+    action=data['action']
+    print('Action:', action)
+    print('Product:',productId)
+
+    customer=request.user.customer
+    product=Product.objects.get(id=productId)
+
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity=(orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity=(orderItem.quantity - 1)
+
+
+    orderItem.save()
+
+    if orderItem <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
