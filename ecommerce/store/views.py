27ecdb2fc945c8ba72ff21422005ba0742055416
Uddhika <uddhikaishara@gmail.com>
2022-02-02 21:00:00 +0530

    guest user functionality finished

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from .models import *
 import json
 import datetime
-from . utils import cookieCart, cartData
+from . utils import cookieCart, cartData, guestOrder
 
 def store(request):
 
@@ -87,24 +87,28 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer=request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total=float(data['form']['total'])
-        order.transaction_id=transaction_id
-
-        if total == order.get_cart_total:
-            order.complete=True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode']
-                )
 
     else:
-        print("user hasn't loggin yet")
+        
+        customer, order = guestOrder(request, data)
+
+    total=float(data['form']['total'])
+    order.transaction_id=transaction_id
+
+    if total == order.get_cart_total:
+        order.complete=True
+    order.save()
+
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode']
+        )
+
 
     return JsonResponse('Payment completed', safe=False)
